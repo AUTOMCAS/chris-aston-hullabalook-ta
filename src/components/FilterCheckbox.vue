bedfb4b96dc52e4dce19395b0ca3547efdcbc1f8
AUTOMCAS <105917288+AUTOMCAS@users.noreply.github.com>
2023-04-13 15:07:24 +0100

    Filter: Extract checkbox to component

---
+++ src/components/FilterCheckbox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <label class="checkbox">
+      <input type="checkbox" v-model="isChecked" @change="handleChange" />
+      <div class="checkbox-label">{{ label }}</div>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilterCheckbox',
+  data() {
+    return {
+      isChecked: this.value,
+    };
+  },
+  props: {
+    label: String,
+  },
+  methods: {
+    handleChange() {
+      this.$emit('checkbox-change', this.isChecked);
+    },
+  },
+};
+</script>
--- src/components/ProductFilters.vue
+++ src/components/ProductFilters.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
-    <label class="checkbox">
-      <input type="checkbox" v-model="isChecked" @change="handleChange" />
-      <div class="checkbox-label">In stock</div>
-    </label>
+  <div class="filter-categories">
+    <FilterCheckbox
+      label="In stock"
+      @checkbox-change="$emit('checkbox-change', $event)"
+    />
   </div>
 </template>
 
 <script>
+import FilterCheckbox from './FilterCheckbox.vue';
 export default {
   name: 'ProductFilters',
+  components: { FilterCheckbox },
+  emits: ['checkbox-change'],
   data() {
     return {
       isChecked: this.value,
